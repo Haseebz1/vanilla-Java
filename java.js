d033431d32a19822f55852127e7c95de6dc81de6
Haseebz1 <haseebkandore@hotmail.com>
2023-05-27 11:10:15 +0100

    Added function to button and Called the id,.class in js

---
+++ java.js
@@ -0,0 +1,10 @@
+var color = ["blue","black","yellow","green"]
+
+var background = document.getElementById("background")
+var btn = document.querySelector(".btn")
+
+console.log(btn)
+btn.addEventListener ('click', function() {
+    console.log(btn)
+
+})
